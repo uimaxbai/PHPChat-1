eaccdad086c0c0f9ef3bbbbeb689bc8e9a7e4001
werdl <116349156+werdl@users.noreply.github.com>
2022-10-26 08:03:52 +0100

    removed sensitive info

--- public_html/db_conn.php
+++ public_html/db_conn.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'id19613881_gonerogue');
-define('DB_PASSWORD', 'GV-PHQ0juIxqN7AD');
-define('DB_NAME', 'id19613881_goneroguedb');
+define('DB_USERNAME', 'hallo');
+define('DB_PASSWORD', 'why you looking');
+define('DB_NAME', 'lol');
  
 /* Attempt to connect to MySQL database */
 $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
